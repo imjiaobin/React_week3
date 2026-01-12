ad67b22ac7b3ce9e30fbb89fdaedab58c43416d5
neyo1009 <neyo1009@gmail.com>
2026-01-12 17:25:20 +0800

    完成進度 - 可以打開modal, 尚未完成 addProduct/Edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState, useEffect, useRef } from 'react';
 import './App.css';
 import axios from 'axios';
-import bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import * as bootstrap from 'bootstrap';
 
 const API_BASE = "https://ec-course-api.hexschool.io/v2";
 const API_PATH = "jiaobin"; 
+
+// ### Modal
+// 1. 建立 tempProduct state
+// 2. 把 Modal 的所有欄位 和 tempProduct 綁定
+// 3. 加上「模式」：新增 or 編輯（isNew / modalMode）
+
+// ### 商品
+// 1. 建立 products state 與取得商品列表的方法
+// 2. 產品 CRUD
+// 3. 啟用/關閉顏色 
 
 function App() {
   const [formData, setFormData] = useState({
@@ -12,7 +23,27 @@ function App() {
     password: "",
   });
   const [isAuth, setisAuth] = useState(false);
+  
+
+  // 1. 建立 tempProduct state
+   const defaultProduct = {
+    title: '',
+    category: '',
+    unit: '',
+    origin_price: '',
+    price: '',
+    description: '',
+    content: '',
+    is_enabled: 0, 
+    imageUrl: '',
+    imagesUrl: [],
+  };
+  const [tempProduct, setTempProduct] =  useState(defaultProduct);
+  // 初始化 product
   const productModalRef = useRef(null);
+  
+
+  // 使用者登入
 
   useEffect(() => {
     const token = document.cookie.replace(
@@ -23,18 +54,18 @@ function App() {
     productModalRef.current = new bootstrap.Modal('#productModal', {
       keyboard: false
     });
-    // checkAdmin();
+    checkAdmin();
     
   }, []);
 
-  // const checkAdmin = async () => {
-  //   try {
-  //     await axios.post(`${API_BASE}/api/user/check`);
-  //     setisAuth(true);
-  //   } catch (err) {
-  //     console.log(err.response.data.message);
-  //   }
-  // };
+  const checkAdmin = async () => {
+    try {
+      await axios.post(`${API_BASE}/api/user/check`);
+      setisAuth(true);
+    } catch (err) {
+      console.log(err.response.data.message);
+    }
+  };
 
   const handleInputChange = (e) => {
     const { id, value } = e.target;
@@ -57,13 +88,38 @@ function App() {
     }
   };
 
+  // 商品列表 (CRUD)
+  // 商品表單
+  const handleProductChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    setTempProduct((prev) => ({
+      ...prev,
+      [name]:
+        type === 'checkbox'
+          ? (checked ? 1 : 0)
+          : type === 'number'
+            ? (value === '' ? '' : Number(value))
+            : value,
+    }));
+  };
+  // 新增表單
+  const addProduct = () => {
+    setTempProduct(defaultProduct);
+    productModalRef.current.show();
+  }
+  const EditProduct = (product) => {
+    setTempProduct(product);
+    productModalRef.current.show();
+  }
+
   return (
     <>
       {isAuth ? (
         <div>
           <div className="container">
             <div className="text-end mt-4">
-              <button className="btn btn-primary">建立新的產品</button>
+              <button className="btn btn-primary" onClick={addProduct}>建立新的產品</button>
             </div>
             <table className="table mt-4">
               <thead>
@@ -173,12 +229,16 @@ function App() {
                         輸入圖片網址
                       </label>
                       <input
+                        id='imageUrl'
                         type="text"
                         className="form-control"
                         placeholder="請輸入圖片連結"
+                        name = 'imageUrl'
+                        value= {tempProduct.imageUrl}
+                        onChange={handleProductChange}
                         />
                     </div>
-                    <img className="img-fluid" src="" alt="" />
+                    <img className="img-fluid" src={tempProduct.imageUrl || null} alt={tempProduct.title || ''} />
                   </div>
                   <div>
                     <button className="btn btn-outline-primary btn-sm d-block w-100">
@@ -199,6 +259,9 @@ function App() {
                       type="text"
                       className="form-control"
                       placeholder="請輸入標題"
+                      name='title'
+                      value={tempProduct.title}
+                      onChange={handleProductChange}
                       />
                   </div>
 
@@ -210,6 +273,9 @@ function App() {
                         type="text"
                         className="form-control"
                         placeholder="請輸入分類"
+                        name = 'category'
+                        value = {tempProduct.category}
+                        onChange={handleProductChange}
                         />
                     </div>
                     <div className="mb-3 col-md-6">
@@ -219,6 +285,9 @@ function App() {
                         type="text"
                         className="form-control"
                         placeholder="請輸入單位"
+                        name='unit'
+                        value={tempProduct.unit}
+                        onChange={handleProductChange}
                         />
                     </div>
                   </div>
@@ -232,6 +301,9 @@ function App() {
                         min="0"
                         className="form-control"
                         placeholder="請輸入原價"
+                        name='origin_price'
+                        value={tempProduct.origin_price}
+                        onChange={handleProductChange}
                         />
                     </div>
                     <div className="mb-3 col-md-6">
@@ -242,6 +314,9 @@ function App() {
                         min="0"
                         className="form-control"
                         placeholder="請輸入售價"
+                        name='price'
+                        value={tempProduct.price}
+                        onChange={handleProductChange}
                         />
                     </div>
                   </div>
@@ -253,6 +328,9 @@ function App() {
                       id="description"
                       className="form-control"
                       placeholder="請輸入產品描述"
+                      name='description'
+                      value={tempProduct.description}
+                      onChange={handleProductChange}
                       ></textarea>
                   </div>
                   <div className="mb-3">
@@ -261,6 +339,9 @@ function App() {
                       id="content"
                       className="form-control"
                       placeholder="請輸入說明內容"
+                      name='content'
+                      value={tempProduct.content}
+                      onChange={handleProductChange}
                       ></textarea>
                   </div>
                   <div className="mb-3">
@@ -269,6 +350,9 @@ function App() {
                         id="is_enabled"
                         className="form-check-input"
                         type="checkbox"
+                        name='is_enabled'
+                        value={tempProduct.is_enabled}
+                        onChange={handleProductChange}
                         />
                       <label className="form-check-label" htmlFor="is_enabled">
                         是否啟用
